43f3c905ba9303edda9fed88003c421948d59eb6
Dat <datltce160481@fpt.edu.vn>
2023-06-08 10:22:15 +0700

    up hihi

---
+++ src/main/java/com/DB/DBConnections.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Group3
+ */
+public class DBConnections {
+
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+ /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        }
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    private final String serverName = "DESKTOP-NT2MFBE\\\\SQLEXPRESS";
+    private final String dbName = "MyShop";
+    private final String portNumber = "1433";
+    private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String userID = "sa";
+    private final String password = "123";
+
+    public static void main(String[] args) {
+        try {
+            // Test connection with database
+            System.out.println(new DBConnections().getConnection());
+        } catch (Exception e) {
+            System.out.println("Not found!");
+        }
+    }
+}
